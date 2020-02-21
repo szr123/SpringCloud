25aff376fe6d429bef952abb96d8318a3961744d
szr <1171140156@qq.com>
2020-02-21 14:59:21 +0800

    配置文件

--- product-view-service-feign/src/main/java/cn/how2j/springcloud/controller/ProductController.java
+++ product-view-service-feign/src/main/java/cn/how2j/springcloud/controller/ProductController.java
@@ -4,12 +4,14 @@ import cn.how2j.springcloud.pojo.Product;
 import cn.how2j.springcloud.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RefreshScope
 @Controller
 public class ProductController {
     @Autowired
